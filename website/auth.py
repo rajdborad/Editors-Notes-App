010808f8c07c931231a4f4de861716a4c414fd25
rajdborad <9512090595raj@gmail.com>
2022-10-22 00:54:14 +0530

    validarion n signup page using flask

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,10 @@
 from xmlrpc.client import boolean
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html", boolean=True)
 
@@ -13,6 +13,26 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    
+    data = request.form
+    print(data)
+    if request.method == 'POST':
+        first_name = request.form.get('firstName')
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+    
+        if len(email) < 4:
+            flash('Email must be 4 character long', category='error')
+        elif len(first_name) < 2:
+            flash('First Name must be 2 character long', category='error')
+        elif password1 != password2:
+            flash('Both password must be same', category='error')
+        elif len(password1) < 8:
+            flash('Password must be 8 character long', category='error')
+        else:
+            flash('Account Created Successfully!...', category='success')
+
     return render_template("sign_up.html")
